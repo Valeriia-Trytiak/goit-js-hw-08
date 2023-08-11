7f1022cebf047e48ac5366acd3a3a9435b9a8fe5
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-08-11 06:50:54 +0200

    update task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,42 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+
+// Создание разметки
+function creatGalleryEl(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `
+          <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+             <img class="gallery__image" src="${preview}" alt="${description}" />
+          </a>
+       </li>
+            `
+    )
+    .join('');
+}
+// Добавление в ДОМ
+galleryContainer.insertAdjacentHTML('beforeend', creatGalleryEl(galleryItems));
+
+// инициализирую библиотеку с описанием и анимацией
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+// Добавление слушателя событий и делегирование
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function onGalleryContainerClick(evt) {
+  // запрешаю переход по ссілке и скачивание файла
+  evt.preventDefault();
+  // открываю модалку
+  // gallery.open();
+}
