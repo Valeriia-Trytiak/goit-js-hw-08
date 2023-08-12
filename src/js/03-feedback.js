b106979377d2a656c0f18a24bcac375eac951350
Valeriia-Trytiak <valeriia.trytiak@gmail.com>
2023-08-12 14:43:24 +0200

    update task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,49 +4,56 @@
 3. При сабмите формы очищай хранилище и поля формы, а также выводи объект с полями email, message и текущими их значениями в консоль.
 4. Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. Для этого добавь в проект и используй библиотеку lodash.throttle. */
 
+//ініціалізую бібліотеку лодеш
 const throttle = require('lodash.throttle');
 
+// забираю елементи форм
 const formEl = document.querySelector('form');
 const inputEmailEl = document.querySelector('input');
 const textareaEl = document.querySelector('textarea');
 
+// викликаю функцію, яка перевіряє локальне сховище та заповнює поля форми даними
 updateFormEl();
 
+// слухаю події форми
 formEl.addEventListener('input', throttle(onFormElInput, 500));
 formEl.addEventListener('submit', onBtnSubmit);
 
+// збираю значення з інпуту та текстерії та додаю до локального сховища
 function onFormElInput() {
   if (inputEmailEl.value !== '' && textareaEl.value !== '') {
     const resultForm = { email: inputEmailEl.value, message: textareaEl.value };
     localStorage.setItem('feedback-form-state', JSON.stringify(resultForm));
     // console.log(resultForm);
   }
-
-  updateFormEl();
 }
 
+// функція, яка перевіряє локальне сховище та заповнює поля форми даними
 function updateFormEl() {
   const values = JSON.parse(localStorage.getItem('feedback-form-state'));
-
-  inputEmailEl.value = values.email || '';
-  textareaEl.value = values.message || '';
+  if (localStorage.length === 0) {
+    return;
+  }
+  inputEmailEl.value = values.email;
+  textareaEl.value = values.message;
 }
 
+//функція відправки форми
 function onBtnSubmit(evt) {
   //  забороняю перезавантаження
   evt.preventDefault();
 
+  // деструтуризую об'єкт елементів
   const {
     elements: { email: emailForm, message: messageForm },
   } = evt.currentTarget;
 
-  // забираю значення пошти та повідомлення
+  // забираю значення пошти та повідомлення і виводжу у консоль
   if (emailForm.value !== '' && messageForm.value !== '') {
     const resultForm = { email: emailForm.value, message: messageForm.value };
 
     console.log(resultForm);
   }
-
   // очищую форму та сховище
   evt.currentTarget.reset();
   localStorage.clear();
